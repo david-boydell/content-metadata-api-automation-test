4c5ab7df9854846d811eda088ed6edd144503ec8
David <david.boydell@bbc.co.uk>
2024-09-13 11:53:18 +0100

    adding prettier config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,8 @@
 import js from '@eslint/js';
+import eslintConfigPrettier from "eslint-config-prettier";
 
-export default [js.configs.recommended];
+
+export default [
+  js.configs.recommended,
+  eslintConfigPrettier,
+];
