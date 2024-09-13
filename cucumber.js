dbba99df2de26d72babad635fdabe96b025735bf
David <david.boydell@bbc.co.uk>
2024-09-13 12:08:13 +0100

    adding prettier config

--- cucumber.js
+++ cucumber.js
@@ -2,5 +2,5 @@ export default {
   worldParameters: {
     baseUrl: process.env.MY_APP_URL || 'https://testapi.io',
   },
-  format: ['html:cucumber-report.html'],
+  format: ['html:cucumber-report.html']
 };
