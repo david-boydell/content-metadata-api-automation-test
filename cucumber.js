4882633424684057baa799cca16b688779fe8a2f
david-boydell <36667923+david-boydell@users.noreply.github.com>
2024-09-17 16:26:13 +0100

    Update cucumber.js

--- cucumber.js
+++ cucumber.js
@@ -1,7 +1,7 @@
 export default {
   worldParameters: {
     baseUrl: process.env.BASE_URL || 'https://testapi.io',
-    responseUpperLimit: 1000,
+    responseUpperLimit: 2000,
   },
   tags: '@run',
   format: ['html:cucumber-report.html'],
